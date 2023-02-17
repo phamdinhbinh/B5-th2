9305a31a27c870acf2758caf063a05dde3d8e575
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-17 11:41:39 +0700

    th2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+
+const Loading = () => {
+  return (
+    <div className='loader'>
+
+    </div>
+  );
+};
 
 function App() {
+const [isLoading, setIsLoading] = useState(true);
+const arr =['0'];
+
+const test = () => {
+   arr[0] =  '1' ;
+  setIsLoading(true);
+  console.log(arr);
+}
+
+useEffect(() => {
+  
+  setTimeout(() => {
+    setIsLoading(false);
+    console.log(arr);
+  },5000);
+
+},arr ); 
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {isLoading && <Loading />}
+
+       <button onClick ={test}>Loading</button>
     </div>
   );
 }
